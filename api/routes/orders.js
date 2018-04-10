4655de53491734a6ae7075e4f522900522f6666a
Mindaugas <mindaugojuskos@gmail.com>
2018-04-10 09:52:46 +0300

    Populate method added to get/orders route

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -9,6 +9,7 @@ const Product = require('../models/product');
 router.get('/', (req, res, next) => {
 	Order.find()
 	.select('product quantity _id')
+	.populate('product', 'name')
 	.exec()
 	.then(docs => {
 		res.status(200).json({
@@ -36,6 +37,11 @@ router.get('/', (req, res, next) => {
 router.post('/', (req, res, next) => {
 		Product.findById(req.body.productId)
 		.then(product => {
+			 if(!product) {
+			 	return res.status(404).json({
+			 		message: "Product not found"
+			 	});
+			 }
 			const order = new Order({
 				_id: mongoose.Types.ObjectId(),
 				quantity: req.body.quantity,
@@ -67,19 +73,44 @@ router.post('/', (req, res, next) => {
 });
 
 router.get('/:orderId', (req, res, next) => {
-	res.status(200);
-	res.send({
-		"message": "Orders details",
-		orderId: req.params.orderId
+	Order.findById(req.params.orderId)
+	.populate('product')
+	.exec()
+	.then(order => {
+		if(!order) {
+			return res.status(404).json({
+				message: "Order not found"
+			});
+		}
+		res.status(200).json({
+			order: order,
+			request: {
+				type: 'GET',
+				url: 'http://localhost:3000/orders/'
+				}
+		});
+	})
+	.catch(err => {
+		res.status(500).json({
+			error: err
+		});
 	});
 });
 
 router.delete('/:orderId', (req, res, next) => {
-	res.status(200);
-	res.send({
-		"message": "Orders deleted",
-		orderId: req.params.orderId
-	});
+	Order.remove({ _id: req.params.orderId })
+	.exec()
+	.then(result => {
+		res.status(200).json({
+			message: 'Order deleted',
+			request: {
+				type: 'POST',
+				url: 'http://localhost:3000/orders/',
+				body: { productId: 'ID', quantity: "Number" }
+				}
+		});
+	})
+	.catch();
 });
 
 
